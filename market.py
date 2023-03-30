0f0666bfba47bb2e7a462fefbcd5211e5b75550b
Abhijith Mallya <abhijithmallya@gmail.com>
2023-03-30 22:22:58 +0530

    sending data to templates

---
+++ market.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+#   package      instance
+
+#initialising Flask instance
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market)
+def market_page:
+    return render_template('market.html')
+     
+
